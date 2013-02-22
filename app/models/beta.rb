f9f7358e2f674304d31a0e20486c4ccc4e4d26ef
Andrew Benson <andebenson@gmail.com>
2013-02-22 13:46:40 -0600

    finished adding beta_answered to beta model

--- app/models/beta.rb
+++ app/models/beta.rb
@@ -1,13 +1,13 @@
 class Beta < ActiveRecord::Base
-  attr_accessible :comment, :route_id, :user_id, :beta_type
+  attr_accessible :comment, :route_id, :user_id, :beta_type, :beta_answered
 
   belongs_to :route
   belongs_to :user 
 
-  validates_presence_of :comment, :route_id, :user_id, :beta_type
+  validates_presence_of :comment, :route_id, :user_id, :beta_type, :beta_answered
 
   def as_json(options={})
-  	super(only: [:id, :comment, :beta_type, :created_at],
+  	super(only: [:id, :comment, :beta_type, :beta_answered, :created_at],
   		  include: {user: {only: [:id, :email, :first_name, :last_name, :admin_to], methods: [:profile_pic_url]},
   		  			route: {only: [:id, :gym_id, :name, :rating, :created_at, :retirement_date, :location, :setter, :route_type]}})
   end

30c95cc519a4ca3f9690e1168fea7ba3b4c6dbd6
Andrew Benson <andebenson@gmail.com>
2013-03-10 23:28:27 -0500

    paperclip now stores profile pictures on AWS (amazon web services) s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ActiveRecord::Base
          :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_pic, :admin_to
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_pic, :admin_to
   # attr_accessible :title, :body
 
   has_many :route_completions, dependent: :destroy
